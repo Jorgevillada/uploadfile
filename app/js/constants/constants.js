d14f68a0e36470b1ccf061a8e34b34d2e402531c
Jorge Villada <jorge-1409@hotmail.com>
2017-10-21 15:48:11 -0500

    url to HTTPS

--- app/js/constants/constants.js
+++ app/js/constants/constants.js
@@ -2,7 +2,7 @@ angular.module('fileupload')
     .constant('Constants', {
         WISTIA_URL_UPLOAD: "https://upload.wistia.com",
         WISTIA_URL_STATUS: "https://api.wistia.com/v1/medias/{0}.json?api_password={1}",
-        WISTIA_URL_IFRAME: "http://fast.wistia.net/embed/iframe/{0}",
+        WISTIA_URL_IFRAME: "https://fast.wistia.net/embed/iframe/{0}",
         WISTIA_PASSWORD: "<%= WISTIA_PASSWORD %>",
         WISTIA_STATUS_PENDING: "pending",
         WISTIA_STATUS_FAILED: "failed",
